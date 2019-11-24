b78d525a67a05c64d825494e8f5577d41ceac227
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2019-11-24 14:03:30 +0300

    Добавил шаблон класса Burger

--- students/Vladislav Ovinkin/other works/lesson_2 (BurgerMaker)/app.js
+++ students/Vladislav Ovinkin/other works/lesson_2 (BurgerMaker)/app.js
@@ -1,2 +1,43 @@
 'use strict';
 
+class Burger {
+    constructor (size, stuffing) {
+
+    }
+
+    // Добавить начинку
+    addTopping (topping) {
+
+    }
+
+    // Убрать начинку
+    removeTopping (topping) {
+
+    }
+
+    // Получить список начинок
+    getToppings (topping) {
+
+    }
+
+    // Узнать размер гамбургера
+    getSize () {
+
+    }
+
+    // Узнать начинку гамбургера
+    getStuffing () {
+
+    }
+
+    // Рассчитать цену
+    calculatePrice () {
+
+    }
+
+    // Рассчитать калорийность
+    calculateCalories () {
+
+    }
+}
+

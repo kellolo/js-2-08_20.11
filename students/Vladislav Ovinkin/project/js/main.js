a3225e210c8104e8944ee2c2ef4ff85199b1a5ee
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2019-12-06 00:32:57 +0300

    Добавил итоговую сумму в корзине. Пришлось переопределить метод render в классе корзины (это дополненный reRender из прошлого коммита) нет, конечно можно в блок корзины засунуть 2 div'а один для списка, другой для суммы, но придётся всё равно писать перерендер для случаев изменения количества и удаления позиций. Рендер из суперкласса хорош только для инициализации, не более того.

--- students/Vladislav Ovinkin/project/js/main.js
+++ students/Vladislav Ovinkin/project/js/main.js
@@ -77,6 +77,7 @@ class ProductsList extends List {
 class CartList extends List {
     constructor (cart, url = '/getBasket.json', container = '.cart-block') {
         super (url, container);
+        this.totalSum = 0;
     }
     _init () {
         this.getJSON (API_URL + this.url)
@@ -118,11 +119,10 @@ class CartList extends List {
                         product.price = +product.dataset['price'];
                         product.quantity = 1;
                         this.DTOarr.push (new CartItem (product));
-                        this.render ();            
                     } else {
                         find.quantity++;
-                        this.reRender ();
                     }
+                    this.render ();
                 } else {
                     throw new Error ('Server error adding item!');
                 }
@@ -140,32 +140,42 @@ class CartList extends List {
                     } else {
                         this.items.splice (this.items.indexOf (find), 1);
                     }
-                    this.reRender ();
+                    this.DTOarr = [];
+                    this.render ();
                 } else {
                     throw new Error ('Server error removing item!')
                 }
             });
     }
-    reRender () { // в случае, если поменялось только количество - перерисовать корзину
+    render () { // из-за итоговой суммы в конце - корзину придётся перерисовывать
         const block = document.querySelector (this.container);
         block.innerText = "";
+        this.totalSum = 0;
+        if (this.DTOarr.length) { // добавим новый элемент в корзину
+            this.DTOarr.forEach (el => {
+                let item = new lists [this.constructor.name] (el);
+                this.items.push (item);
+            });
+        }
         this.items.forEach (el => {
             let item = new lists [this.constructor.name] (el);
             block.insertAdjacentHTML ('beforeend', item.render());
+            this.totalSum += el.quantity * el.price;
         });
+        this.renderTotalSum (block);
     }
-    // _getRequestAdd () {
-    //     return makeGETRequestPromise (`${API_URL}/addToBasket.json`);
-    // }
-    // _getRequestRemove () {
-    //     return makeGETRequestPromise (`${API_URL}/deleteFromBasket.json`);
-    // }
+    renderTotalSum (block) {
+        if (this.totalSum) {
+            let code = `<div class='cart-total'>Итого: ${this.totalSum}</div>`;
+            block.insertAdjacentHTML ('beforeend', code);
+        }
+    }
 }
 
 let basket = new CartList ();
 let products = new ProductsList (basket);
 
-class ProductItem extends ListItem{ 
+class ProductItem extends ListItem { 
 
 }
 
@@ -197,199 +207,3 @@ const lists = { // словарь
     ProductsList: ProductItem,
     CartList: CartItem,
 }
-
-
-// class Product {
-//     constructor (product) {
-//         this.product_name = product.product_name;
-//         this.product_id = product.product_id;
-//         this.img = product.img;
-//         this.price = product.price;
-//     }
-//     render () {
-//         return `<div class="product-item" data-product_id="${this.product_id}">
-//                     <img src="${this.img}" alt="Some img">
-//                     <div class="desc">
-//                         <h3>${this.product_name}</h3>
-//                         <p>${this.price} $</p>
-//                         <button class="buy-btn" 
-//                         data-product_id="${this.product_id}"
-//                         data-product_name="${this.product_name}"
-//                         data-image="${this.img}"
-//                         data-price="${this.price}">Купить</button>
-//                     </div>
-//                 </div>`
-//     }
-// }
-
-// class Products {
-//     constructor (block) {
-//         this.products = [];
-//         this.block = `.${block}`;
-//         this._init ();
-//     }
-//     _init () {
-//         this._fetchGoods()
-//             .then (data => {this._fillProducts(data)})
-//             .then (() => this.render ());
-//     }
-//     _fetchGoods () {
-//         return makeGETRequestPromise (`${API_URL}/catalogData.json`);
-//     }
-//     _fillProducts (data) {
-//         let dataArr = JSON.parse (data);
-//         dataArr.forEach (item => {
-//             this.products.push (new Product (item));
-//         });
-//     }  
-//     render () {
-//         const block = document.querySelector (this.block);
-//         let str = '';
-//         this.products.forEach (item => {
-//             str += item.render ();
-//         });
-//         block.innerHTML = str;
-//     }
-// }
-// class CartItem {
-//     constructor (product) {
-//         this.product_id = product.product_id;
-//         this.product_name = product.product_name;
-//         this.price = product.price;
-//         this.img = cartImage;
-//         this.quantity = 1;
-//     }
-// }
-
-// class Cart {
-//     constructor (block) {
-//         this._addEventsListeners ();
-//         this.products = [];
-//         this.block = `.${block}`;
-//         this._init ();
-//     }
-//     _init () {
-//         this._fetchGoods()
-//             .then (data => {this._fillProducts(data)})
-//             .then (() => this.render ());
-//     }
-//     _fetchGoods () {
-//         return makeGETRequestPromise (`${API_URL}/getBasket.json`);
-//     }
-//     _fillProducts (data) {
-//         let dataArr = JSON.parse (data);
-//         let arr = dataArr.contents;
-//         arr.forEach (item => {
-//             this.products.push (new CartItem (item)); 
-//         });
-//     }
-//     _addEventsListeners () {
-//         document.querySelector('.cart-block').addEventListener ('click', (evt) => {
-//             if (evt.target.classList.contains ('del-btn')) {
-//                 this.removeItem (evt.target);
-//             }
-//         })
-        
-//         document.querySelector('.products').addEventListener ('click', (evt) => {
-//             if (evt.target.classList.contains ('buy-btn')) {
-//                 this.addItem (evt.target);
-//             }
-//         })
-//     }
-//     _getRequestAdd () {
-//         return makeGETRequestPromise (`${API_URL}/addToBasket.json`);
-//     }
-//     _getRequestRemove () {
-//         return makeGETRequestPromise (`${API_URL}/deleteFromBasket.json`);
-//     }
-//     addItem (product) {
-//         this._getRequestAdd ()
-//             .then (result => {
-//                 const answer = JSON.parse(result);
-//                 return answer.result;
-//             })
-//             .then (result => {
-//                 if (result == 1) {
-//                     const id = +product.dataset['product_id'];
-//                     const find = this.products.find (element => element.product_id === id);
-//                     if (!find) {
-//                         product.product_id = id;
-//                         product.product_name = product.dataset['product_name'];
-//                         product.price = +product.dataset['price'];
-//                         this.products.push (new CartItem (product));
-//                     } else {
-//                         find.quantity++;
-//                     }
-//                     this.render ();            
-//                 } else {
-//                     throw new Error ('Server error adding item!');
-//                 }
-//             });
-//     }
-//     removeItem (product) {
-//         this._getRequestRemove ()
-//             .then (result => {
-//                 const answer = JSON.parse(result);
-//                 return answer.result;
-//             })
-//             .then (result => {
-//                 if (result == 1) {
-//                     const id = +product.dataset['product_id'];
-//                     const find = this.products.find (element => element.product_id === id);
-//                     if (find.quantity > 1) {
-//                         find.quantity--;
-//                     } else {
-//                         this.products.splice (this.products.indexOf (find), 1);
-//                     }
-//                     this.render ();
-//                 } else {
-//                     throw new Error ('Server error removing item!')
-//                 }
-//             });
-//     }
-//     render () {
-//         this.totalSum = 0;
-//         const block = document.querySelector (this.block);
-//         let code = '';
-//         this.products.forEach (el => {
-//             code += `<div class="cart-item" data-product_id="${el.product_id}">
-//                         <div class="product-bio">
-//                             <img src="${el.img}" alt="Some image">
-//                             <div class="product-desc">
-//                                 <p class="product-title">${el.product_name}</p>
-//                                 <p class="product-quantity">Quantity: ${el.quantity}</p>
-//                                 <p class="product-single-price">$${el.price} each</p>
-//                             </div>
-//                         </div>
-//                         <div class="right-block">
-//                             <p class="product-price">${el.quantity * el.price}</p>
-//                             <button class="del-btn" data-product_id="${el.product_id}">&times;</button>
-//                         </div>
-//                     </div>`;
-//             this.totalSum += el.quantity * el.price;
-//         });
-//         (this.totalSum) ? code += `<div class='cart-total'>Итого: ${this.totalSum}</div>` : code;
-//         block.innerHTML = code;
-//     }
-// }
-
-// let catalog = new Products ('products');
-// let basket = new Cart ('cart-block');
-
-// function makeGETRequestPromise (url) {
-//     return new Promise ((res, rej) => {
-//         let xhr = new XMLHttpRequest ();
-//         xhr.onreadystatechange = function () {
-//             if (xhr.readyState === 4) {
-//                 if (xhr.status == 200) {
-//                     res (xhr.responseText);
-//                 }
-//                 else {
-//                     rej (new Error(`Get data ERROR! Data file: ${url}`));
-//                 }
-//             }
-//         }
-//         xhr.open ('GET', url, true);
-//         xhr.send ();
-//     })
-// }

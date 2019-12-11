87ac65b68f56f11ad5556b7cfbbcfca911fbeb74
invector4ik002 <Vedoone@mail.ru>
2019-12-11 17:51:39 +0700

    11.12.19

--- students/Michail Devichensky/project/script/test.js
+++ students/Michail Devichensky/project/script/test.js
@@ -1,3 +1,36 @@
+<<<<<<< HEAD
+let products = new Vue ({
+    el: '.products',
+    data: {
+        DTOarr:
+        // arr:[
+        //     {
+        //       "id_product": 1,
+        //       "img": "https://placehold.it/200x150",
+        //       "price": 1000,
+        //       "quantity": 0,
+        //       "product_name": "Notebook"
+        //     }     
+        // ]
+        
+    },
+    // methods: {
+    //     getJSON(url) { //аформление запросов  
+    //         return fetch(url)
+    //         .then(d => d.json())
+    //         .catch(err => {console.log(err)})
+    //         .finally((data) => this.DTOarr = data)
+    //     }
+    // },
+    // // computed: {
+        
+    //     // },
+    //     mounted() {
+    //         this.getJSON('https://raw.githubusercontent.com/invector4ik002/myJSON/master/myJSON.json') 
+    //         .then(data => this.DTOarr = data)    
+    // }
+})
+=======
 // let url_items = 'https://raw.githubusercontent.com/invector4ik002/myJSON/master/myJSON.json'
 // let arrItems
 // fetch(url_items)
@@ -19,3 +52,4 @@ let add = new Vue ({
 
     }
 })
+>>>>>>> 589a7a787fe2623a841f399a2d0a5e478b6614b5

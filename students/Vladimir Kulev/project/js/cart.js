9578dbb7e25ea651fc95b728cf194906d07e609d
VladimirKul <vova12kul@gmail.ru>
2019-12-11 10:40:50 +0300

    contacts

--- students/Vladimir Kulev/project/js/cart.js
+++ students/Vladimir Kulev/project/js/cart.js
@@ -1,83 +1,75 @@
 let doc = document
 
-class fillProduct {
-    constructor() {
-        this.url = 'https://raw.githubusercontent.com/VladimirKul/online-store-api/master/responses/catalogData.json'
-        this.product = []
-        this.getRequest(this.url)
+const image = 'http://placehold.it/200x200'
+const cart_image = 'http://placehold.it/75x75'
+const FAKEAPI = 'https://raw.githubusercontent.com/VladimirKul/online-store-api/master/responses'
+
+class List {
+    constructor(url, container) {
+        this.url = url
+        this.container = container
+
+        this.products = []
+        this.productsDTO = []
+
+        this._init()
     }
 
-    getRequest(url) {
-        fetch(url).then(data => data.json()).then(data => {
-            this.product = data
-            new renderCatalog(this.product)
+    getJSON(url) {
+        return fetch(url).then(data => data.json())
+    }
+
+    _init() {
+        this.getJSON(FAKEAPI + this.url).then(d => {
+            this.productsDTO = d
         })
     }
 
-    filterProduct(evt) {
-        if (evt.target.classList.contains('btn-filter')) {
-            let inp_filter = doc.querySelectorAll('.inp-filter')
-            let arr_rad = doc.querySelectorAll('.radio-sort')
-            let arr_filter = catalog.product.concat()
-            let inp_filter_sec = +inp_filter[0].value
-            let inp_filter_fir = +inp_filter[1].value
-
-            if (inp_filter_fir == 0) {
-                inp_filter_fir = 100000000
-            }
-
-
-            let filtered = arr_filter.filter((item => {
-                return item.price >= inp_filter_sec && item.price <= inp_filter_fir
-            }))
-
-            if (arr_rad[0].checked) {
-                filtered.sort((prev, next) => prev.price - next.price)
-            }
-
-            if (arr_rad[1].checked) {
-                filtered.sort((prev, next) => next.price - prev.price)
-            }
-
-            new renderCatalog(filtered)
-        }
-    }
-
-}
-
-class renderCatalog {
-    constructor(arr) {
-        this._render(arr)
-    }
-
-    _render(arr) {
-        doc.querySelector('.wrap-product').innerHTML = ''
-        let i = 0
-        for (i in arr) {
-            doc.querySelector('.wrap-product').innerHTML += `
-                <div class="product-item">
-                <img src="http://placehold.it/200x200" alt="img" class="img-product-item">
-                <p class="name-product-item">${arr[i].name}</p>
-                <p class="price-product-item">${arr[i].price} &#36;</p>
-                <input type="button" value="BUY" class="btn-product-item"
-                    data-id="${arr[i].id}" data-name="${arr[i].name}" data-price="${arr[i].price}">
-                </div>
-            `
-        }
+    render() {
+        const block = doc.querySelector(this.container)
+        this.productsDTO.forEach(el => {
+            let product = new lists[this.constructor.name](el)
+            this.products.push(product)
+            block.insertAdjacentHTML('beforeEnd', product.render())
+        })
     }
 }
 
-class fillCart {
-    constructor() {
-        this.arrCart = []
+class ProductsList extends List {
+    constructor(url = '/catalogData.json', container = '.wrap-product') {
+        super(url, container)
+    }
+
+    _init() {
+        this.getJSON(FAKEAPI + this.url).then(d => {
+            this.productsDTO = d
+            this.render()
+        })
+    }
+}
+
+class CartList extends List {
+    constructor(url = '/getBasket.json', container = '.wrap-cart') {
+        super(url, container)
         this.sumCart = null
+    }
+
+    render() {
+        const block = doc.querySelector(this.container)
+        this.products = []
+        this.productsDTO.forEach(el => {
+            let product = new lists[this.constructor.name](el)
+            this.products.push(product)
+            block.insertAdjacentHTML('beforeEnd', product.render())
+        })
+        block.insertAdjacentHTML('beforeEnd', `<h2 class="amount">Total amount: ${cartCatalog.sumCart}&#36; <input type="button" value="X" class="close-cart"></h2>`)
     }
 
     addProductCart(evt) {
         let target = evt.target
         let btn_buy_target = target.classList.contains('btn-product-item')
         if (btn_buy_target) {
-            let find_el = cart.arrCart.find(function(el) {
+            let find_el = cartCatalog.productsDTO.find(function(el) {
                 if (el.id == target.dataset.id) {
                     return el
                 }
@@ -86,10 +78,17 @@ class fillCart {
             if (find_el) {
                 find_el.count++
             } else {
-                cart.arrCart.push(new fillProductItem(target.dataset.id, target.dataset.name, target.dataset.price))
+                let objItem = {
+                    id: target.dataset.id,
+                    name: target.dataset.name,
+                    price: target.dataset.price,
+                    count: 1
+                }
+                cartCatalog.productsDTO.push(new ListItem(objItem))
             }
-            cart.sumCart += +target.dataset.price
-            doc.querySelector('.wrap-cart').innerHTML = `<input type="button" value="X" class="close-cart">` + new RenderCart(cart.arrCart).str + `<h2 class="amount">Total amount: ${cart.sumCart}&#36; </h2> `
+            cartCatalog.sumCart += +target.dataset.price
+            doc.querySelector(cartCatalog.container).innerHTML = ''
+            cartCatalog.render()
         }
     }
 
@@ -97,7 +96,7 @@ class fillCart {
         let target = evt.target
         let btn_buy_target = target.classList.contains('btn-delete-product')
         if (btn_buy_target) {
-            let find_el = cart.arrCart.find((el) => {
+            let find_el = cartCatalog.productsDTO.find((el) => {
                 if (el.id == target.dataset.id) {
                     return el
                 }
@@ -106,10 +105,11 @@ class fillCart {
             if (find_el.count > 1) {
                 find_el.count--
             } else {
-                cart.arrCart.splice(cart.arrCart.indexOf(find_el), 1)
+                cartCatalog.productsDTO.splice(cartCatalog.productsDTO.indexOf(find_el), 1)
             }
-            cart.sumCart -= +target.dataset.price
-            doc.querySelector('.wrap-cart').innerHTML = `<input type="button" value="X" class="close-cart">` + new RenderCart(cart.arrCart).str + `<h2>Total amount: ${cart.sumCart}&#36; </h2> `
+            cartCatalog.sumCart -= +target.dataset.price
+            doc.querySelector(cartCatalog.container).innerHTML = ''
+            cartCatalog.render()
 
         }
     }
@@ -125,56 +125,99 @@ class fillCart {
             vis_cart.style.visibility = 'hidden';
         }
     }
-
 }
 
-class RenderCart {
-    constructor(arr) {
-        this.str = this._render(arr)
+class ListItem {
+    constructor(el) {
+        this.name = el.name
+        this.price = el.price
+        this.id = el.id
+        this.count = el.count
     }
-    _render(cart) {
-        let str = ''
-        cart.forEach((el) => {
-            str += `
-            <div class="cartt">
-            <div class="wrap-item-cart">
-                <div class="wrap-desc">
-                    <img src="http://placehold.it/75x75" alt="img">
-                    <p class="cart-name-product">${el.name}</p>
-                    <p class="cart-price-prduct">${el.price} &#36;</p>
+
+    render() {
+        return `
+            <div class="product-item">
+            <img src="http://placehold.it/200x200" alt="img" class="img-product-item">
+            <p class="name-product-item">${this.name}</p>
+            <p class="price-product-item">${this.price} &#36;</p>
+            <input type="button" value="BUY" class="btn-product-item"
+                data-id="${this.id}" data-name="${this.name}" data-price="${this.price}">
+            </div>
+        `
+    }
+}
+
+class ProductItem extends ListItem {}
+
+class CartItem extends ListItem {
+    render() {
+        return `
+                <div class="cartt">
+                    <div class="wrap-item-cart">
+                        <div class="wrap-desc">
+                            <img src="http://placehold.it/75x75" alt="img">
+                            <p class="cart-name-product">${this.name}</p>
+                            <p class="cart-price-prduct">${this.price} &#36;</p>
+                        </div>
+                        <div class="wrap-calc">
+                            <p class="cart-count-prduct">count: ${this.count}</p>
+                            <p class="cart-sum-product">amount: ${this.price*this.count}</p>
+                            <input type="button" value="+" class="btn-product-item"
+                            data-id="${this.id}" data-name="${this.name}" data-price="${this.price}">
+                            <input type="button" value="-" class="btn-delete-product"
+                            data-id="${this.id}" data-name="${this.name}" data-price="${this.price}">
+                        </div> 
+                    </div>
+        
                 </div>
-                <div class="wrap-calc">
-                    <p class="cart-count-prduct">count: ${el.count}</p>
-                    <p class="cart-sum-product">amount: ${el.price*el.count}</p>
-                    <input type="button" value="+" class="btn-product-item"
-                        data-id="${el.id}" data-name="${el.name}" data-price="${el.price}">
-                    <input type="button" value="-" class="btn-delete-product"
-                        data-id="${el.id}" data-name="${el.name}" data-price="${el.price}">
-                </div> 
-            </div>
-            
-            </div>
             `
-        })
-        return str
     }
 }
 
-class fillProductItem {
-    constructor(id, name, price) {
-        this.id = id
-        this.name = name
-        this.price = price
-        this.count = 1
-    }
+const lists = {
+    ProductsList: ProductItem,
+    CartList: CartItem
 }
 
-let catalog = new fillProduct
-let cart = new fillCart
 
-doc.querySelector('.wrap-product').addEventListener('click', cart.addProductCart)
-doc.querySelector('.wrap-cart').addEventListener('click', cart.addProductCart)
-doc.querySelector('.wrap-cart').addEventListener('click', cart.subProductCart)
-doc.querySelector('.btn-cart').addEventListener('click', cart.visibilityCart)
-doc.querySelector('.wrap-cart').addEventListener('click', cart.closeCart)
-doc.querySelector('.btn-filter').addEventListener('click', catalog.filterProduct)
+let productsCatalog = new ProductsList
+let cartCatalog = new CartList
+
+
+
+
+//     filterProduct(evt) {
+//         let arr_rad = doc.querySelectorAll('.radio-sort')
+//         let inp_filter = doc.querySelectorAll('.inp-filter')
+//         let arr_filter = catalog.product.concat()
+//         let inp_filter_sec = +inp_filter[0].value
+//         let inp_filter_fir = +inp_filter[1].value
+
+//         if (evt.target.classList.contains('btn-filter') || evt.target.classList.contains('radio-sort')) {
+//             if (inp_filter_fir == 0) {
+//                 inp_filter_fir = 100000000
+//             }
+
+//             let filtered = arr_filter.filter((item => {
+//                 return item.price >= inp_filter_sec && item.price <= inp_filter_fir
+//             }))
+
+//             if (arr_rad[0].checked) {
+//                 filtered.sort((prev, next) => prev.price - next.price)
+//             }
+
+//             if (arr_rad[1].checked) {
+//                 filtered.sort((prev, next) => next.price - prev.price)
+//             }
+
+//             new renderCatalog(filtered)
+//         }
+//     }
+
+doc.querySelector('.wrap-product').addEventListener('click', cartCatalog.addProductCart)
+doc.querySelector('.wrap-cart').addEventListener('click', cartCatalog.addProductCart)
+doc.querySelector('.wrap-cart').addEventListener('click', cartCatalog.subProductCart)
+doc.querySelector('.btn-cart').addEventListener('click', cartCatalog.visibilityCart)
+doc.querySelector('.wrap-cart').addEventListener('click', cartCatalog.closeCart)
+    // doc.querySelector('.wrap-f').addEventListener('click', catalog.filterProduct)

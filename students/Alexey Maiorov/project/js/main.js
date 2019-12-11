84a30468ee2749feffb143207ce47b0b733e9691
alexmaiorov <alexeymajor0@gmail.com>
2019-12-11 12:48:41 +0300

    Выполнена разбивка на компоненты. Добавлен поиск.

--- students/Alexey Maiorov/project/js/main.js
+++ students/Alexey Maiorov/project/js/main.js
@@ -17,35 +17,5 @@ let app = new Vue ({
             return fetch (url)
                 .then (d => d.json ())
         },
-        showCart (){
-            this.cartInvisible = !this.cartInvisible
-        },
-        addProduct(event) {
-            let productId = +event.target.dataset['id']
-            let find = this.cart.find (element => element.id === productId)
-            if (!find) {
-                this.cart.push ({
-                    id: this.products[productId-1].id,
-                    title: this.products[productId-1].title,
-                    price: this.products[productId-1].price,
-                    quantity: 1
-                })
-            } else {
-                find.quantity++
-            }
-        },
-        removeProduct (event){
-            let productId = +event.target.dataset['id']
-            let find = this.cart.find (element => element.id === productId)
-            if (find.quantity > 1) {
-                find.quantity--;
-            } else {
-                this.cart.splice(this.cart.indexOf(find), 1);
-            }
-        }
-    },
-    mounted () {
-        this.getJSON(this.dataURL)
-            .then(d => this.products = d)
     }
 })

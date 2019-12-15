974a73f35d69c7c5475aa514b1e1fc7272198767
Vladislav Ovinkin <54041894+vladovinkin@users.noreply.github.com>
2019-12-15 23:24:30 +0300

    Update CartItem.js
    Совсем по мелочи синтаксис конкатенации в "усах".

--- students/Vladislav Ovinkin/project/js/CartItem.js
+++ students/Vladislav Ovinkin/project/js/CartItem.js
@@ -12,11 +12,11 @@ Vue.component ('cart-item', {
                 <div class="product-desc">
                     <p class="product-title">{{ el.product_name }}</p>
                     <p class="product-quantity">Quantity: {{ el.quantity }}</p>
-                    <p class="product-single-price">{{'$'+el.price }} each</p>
+                    <p class="product-single-price">{{'$' + el.price }} each</p>
                 </div>
             </div>
             <div class="right-block">
-                <p class="product-price">{{'$'+el.quantity * el.price }}</p>
+                <p class="product-price">{{'$' + el.quantity * el.price }}</p>
                 <button class="del-btn"
                     @click="$parent.removeProduct(el)">&times;</button>
             </div>
